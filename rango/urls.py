dc237c8697e0a2017c5b5f2abeccd20406312147
sherrycherish <617295898@qq.com>
2016-06-21 15:42:23 +0800

    tango
    skeleton

---
+++ rango/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls import patterns, url
+from rango import views
+from django.conf import settings  # New Import
+from django.conf.urls.static import static  # New Import
+
+urlpatterns = patterns('',
+                       url(r'^$', views.index, name='index'),
+                       url(r'^about/', views.about, name='about'),
+                       url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.category, name='category'),
+                       # url(r'^category/(?P<category_name_slug>[\w\-]+)/add_page/$', views.add_page, name='add_page'),
+                       url(r'^add_category/$', views.add_category, name='add_category'),
+                       url(r'^register/$', views.register, name='register'),
+                       url(r'^login/$', views.user_login, name='login'),
+                       url(r'^logout/$', views.user_logout, name='logout'),
+                       )
+
+if not settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
